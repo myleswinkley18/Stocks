fd46bd9070cf858a71fae9605fa431b36844ec75
Myles Winkley <myleswinkley@gmail.com>
2016-06-04 16:04:10 -0400

    First Commit

---
+++ sandbox/src/main/java/Medmyne/sandbox/App.java
@@ -0,0 +1,13 @@
+package Medmyne.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
