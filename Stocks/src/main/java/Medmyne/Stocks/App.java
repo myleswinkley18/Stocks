e2a623a4ea7247c32357e6f3c26f25c8bae43dee
Myles Winkley <myleswinkley@gmail.com>
2016-08-04 09:34:33 -0400

    Initial Commit

--- Stocks/src/main/java/Medmyne/Stocks/App.java
+++ Stocks/src/main/java/Medmyne/Stocks/App.java
@@ -1,4 +1,4 @@
-package Analysis.Stocks;
+package Medmyne.Stocks;
 
 /**
  * Hello world!
--- Stocks/src/test/java/Medmyne/Stocks/AppTest.java
+++ Stocks/src/test/java/Medmyne/Stocks/AppTest.java
@@ -1,4 +1,4 @@
-package Analysis.Stocks;
+package Medmyne.Stocks;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
